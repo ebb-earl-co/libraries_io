cc8a8642b1cd7dfd20cd9ac2c9a7bbf653c5389f
ebb-earl-co <eberlcoecolin@gmail.com>
2019-09-10 15:24:42 -0600

    Add python/ directory with pipenv files, .py files

---
+++ python/get_pypi_python_projects_from_neo4j.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+This script requests the Libraries.io API Project Contributors endpoint. Pass
+the name of the project the contributors of which to be requested as the only
+positional argument; e.g.
+
+>>> python %s 'MATCH (p:Project) return p;'
+
+If the request is successful, the JSON response is returned to STDOUT. Otherwise
+the error that occurred during the request is returned in JSON format to STDOUT.
+"""
+
+from neo4j import DirectDriver
+from neo4j.exceptions import CypherError
+
+from contextlib import closing
+from getpass import getpass
+import json
+import sys
+
+
+URI = "bolt://localhost:7687"
+
+
+def get_neo4j_driver(uri, auth, **kwargs):
+    """ Given URI to connect to, `uri`, and authentication info,
+    `auth`, (as well as optional kwargs passed to neo4j.GraphDatabase.Driver)
+    return a neo4j.Driver object to interact with the graph hosted at `uri`
+
+    Args:
+        uri (str): address at which Neo4j graph is accessed
+        auth (tuple): (username, password) for graph at `uri`
+    Returns:
+        (neo4j.Driver): driver object to interact with graph at `uri`
+    """
+    return DirectDriver(uri, auth=auth)
+
+
+def execute_cypher_query(driver, query, params=None):
+    """ Given `neo4j.Driver` instance and `query` str, execute `query` via
+    the `driver` in a session, returning the `neo4j.BoltStatementResult` object
+    that results.
+
+    Args:
+        driver (neo4j.Driver): instance of database driver
+        query (str): Cypher query to execute
+        params (dict): Neo4j parameters that are substituted into `query`
+    Returns:
+        (neo4j.BoltStatementResult): the result object
+    """
+    with driver.session() as session:
+        result = session.run(query, parameters=params)
+        return result
+
+
+def return_iterator_of_node_properties(res, prop):
+    """ Given `neo4j.BoltStatementResult` object, return the
+    property `property` of each node in `res` as an iterator.
+    If `res` is empty or upon error, output is `iter()`.
+
+    Args:
+        res (neo4j.BoltStatementResult): result of query
+        prop (str): property to get from Neo4j node(s) in `res`
+    Returns:
+        (iterator): `node.property for node in res` or `iter()`
+    """
+    if res.peek() is None:
+        return iter(())
+    else:
+        return (record.get(prop) for record in res.records())
+
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+
+    if '-h' in argv or '--help' in argv:
+        print(__doc__ % argv[0], sys.stderr)
+        return 0
+
+    try:
+        query = argv[1]
+    except IndexError:
+        print("No Cypher query passed", sys.stderr)
+        return 1
+
+    authentication = ('neo4j', getpass('Graph DB password:'))
+    with closing(get_neo4j_driver(URI, authentication)) as driver:
+        try:
+            result = execute_cypher_query(driver, query)
+        except CypherError as c:
+            to_return = json.dumps({"CypherError": str(c)})
+            print("Cypher error occurred", sys.stderr)
+            return to_return
+        except Exception as e:
+            to_return = json.dumps({"Exception": str(e)})
+            print("Exception occurred", sys.stderr)
+            return to_return
+        else:
+            to_return = (record.get('p').get('name') for record in result.records())
+
+        return json.dumps(list(to_return))
+
+if __name__ == "__main__":
+    sys.stdout.write(main())
---
+++ python/request_libraries_io_project_contributors.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+This script requests the Libraries.io API Project Contributors endpoint. Pass
+the name of the project the contributors of which to be requested as the only
+positional argument; e.g.
+
+>>> python %s requests
+
+If the request is successful, the JSON response is returned to STDOUT. Otherwise
+the error that occurred during the request is returned in JSON format to STDOUT.
+"""
+
+from requests import get as GET
+from requests.exceptions import HTTPError
+
+import json
+import os
+import sys
+
+URL = "https://libraries.io/api/Pypi/%s/contributors"
+
+
+def build_GET_request(url):
+    """ Given url, return requests.get object that is prepared with
+    the keywords 'url', 'params' passed. N.b. the params are created
+    internally because they are taken from ENV variables or user input.
+
+    Args:
+        url (str): the url to pass to requests.get; the API to request
+    Returns:
+        (requests.get): the requests.get object for further processing
+    """
+    def get_api_key():
+        api_key = os.environ.get("APIKEY")
+        return api_key
+
+    params = {"api_key": get_api_key()}
+    return GET(url=url, params=params)
+
+
+def execute_GET_request(r):
+    """ Given a `requests.get` object, execute the GET request and return one of
+    three JSON objects: 1) requests.HTTPError that arose; 2) Other Python
+    exception that arose; or 3) The response in the form {"data": <response>}
+    from the API.
+
+    Args:
+        r (requests.get): instantiated request object
+    Returns:
+        (str): JSON-formatted string
+    """
+    try:
+        r.raise_for_status()
+    except HTTPError as h:
+        to_return = json.dumps({"HTTPError": str(h)})
+    except Exception as e:
+        to_return = json.dumps({"Exception": str(e)})
+    else:
+        to_return = json.dumps({"data": json.loads(r.content)})
+    finally:
+        r.close()
+
+    return to_return
+
+
+def main(argv=None):
+    # Argument parsing
+    if argv is None:
+        argv = sys.argv
+
+    if '-h' in argv or '--help' in argv:
+        print(__doc__ % argv[0], sys.stderr)
+        return 0
+
+    try:
+        project_name = argv[1]
+    except IndexError:
+        print("Project name not passed", sys.stderr)
+        return 1
+
+    # Requesting from API
+    GET_request = build_GET_request(URL % project_name)
+    return execute_GET_request(GET_request)
+
+if __name__ == '__main__':
+    sys.stdout.write(main())
