6c5eadd4d0efd2a29a4a900830a35a0acd7b403a
ebb-earl-co <eberlcoecolin@gmail.com>
2019-09-10 16:58:36 -0600

    Multiple updates:
    Add create_sqlite_db.py to store info about what API calls have been made
    Modify other .py files to specify file=sys.stderr in print() calls

---
+++ python/create_sqlite_db.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from sqlite3 import connect, OperationalError
+import sys
+
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+
+    try:
+        dbname = argv[1]
+    except IndexError:
+        print("Database name not passed", file=sys.stderr)
+        return 1
+
+    conn = connect(dbname)
+
+    c = conn.cursor()
+    try:
+        c.execute("""create table project_names (
+            project_name text unique not null,
+            api_has_been_queried integer,
+            api_query_succeeded integer,
+            execution_error text,
+            contributors blob,
+            ts timestamp)""")
+    except OperationalError:
+        print("Table has already been created", file=sys.stderr)
+        return 0
+    except Exception as e:
+        print(str(e), file=sys.stderr)
+        return 1
+    else:
+        print("Table created successfully", file=sys.stderr)
+        return 0
+    finally:
+        c.close()
+        conn.close()
+
+if __name__ == "__main__":
+    sys.exit(main())
