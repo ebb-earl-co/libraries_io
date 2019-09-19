c37a63e6cbf4d6ca106c1b0ba81a2a21acdb0e41
ebb-earl-co <eberlcoecolin@gmail.com>
2019-09-19 10:06:21 -0700

    Fix syntax errors and give new names to placeholder variables

--- python/merge_contributors.py
+++ python/merge_contributors.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from logger import return_logger
+import logging
+from contextlib import closing
 
-logger = return_logger(__name__)
+logger = logging.getLogger(__name__)
 
 # Merge contributors from JSON returned by Libraries.io API
 query = """MERGE (c:Contributor {uuid: apoc.convert.toInteger($uuid)})
@@ -30,9 +31,9 @@ def merge_contributors(driver, project_name, query, contributors):
         (dict): {`project_name`: [{contributor, successful, query, params}]}
     """
     output_array = []
-    with closing(neo4j_driver) as driver:
+    with closing(driver) as d:
         logger.info(f"Connected to Neo4j running on {driver.address}")
-        with driver.session() as session:
+        with driver.session() as s:
             for i, contributor in enumerate(contributors, 1):
                 cypher_params = get_contributor_keys_from_dict(contributor)
                 logger.debug(f"MERGEing contributor {i} "
@@ -40,12 +41,12 @@ def merge_contributors(driver, project_name, query, contributors):
                              f"{project_name} using query:"
                              f"\n{query}\n and parameters:\n"
                              f"{cypher_params}")
-                with session.begin_transaction() as tx:
+                with s.begin_transaction() as tx:
                     try:
                         tx.run(merge_contributors_query, cypher_params)
                     except CypherError:
                         logger.error(f"COULD NOT MERGE contributor {i} "
-                                     f"(out of {len(contributors}) for project "
+                                     f"(out of {len(contributors)} for project "
                                      f"{project_name}, corresponding to\n"
                                      f"{contributor}", exc_info=True)
                         tx.rollback()
@@ -69,4 +70,4 @@ def merge_contributors(driver, project_name, query, contributors):
             return {"project_name": project_name,
                     "result": output_array,
                     "has_contributor_error": \
-                        any(map(lambda d: not d['successful'], output_array}
+                        any(map(lambda d: not d['successful'], output_array))}
