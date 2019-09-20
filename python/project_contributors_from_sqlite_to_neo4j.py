d81d6707e4ab1bc5bf657f3a037e8e3da11d23fc
ebb-earl-co <eberlcoecolin@gmail.com>
2019-09-19 19:08:56 -0700

    Fix Cypher queries in project_names_to...py

--- python/project_contributors_from_sqlite_to_neo4j.py
+++ python/project_contributors_from_sqlite_to_neo4j.py
@@ -20,63 +20,64 @@ def main():
 
     select_query = f"""select project_name, contributors from {args.table}
     where api_has_been_queried=1 and api_query_succeeded=1 and
-    merge_contributors_succeeded is null;"""
+    merge_contributors_succeeded is null"""
     with connect(args.DB) as conn:
         conn.row_factory = Row
         select_query_result = select_from_sqlite(conn, select_query)
+        # output is a list of tuples
 
+    # Convert list of tuples to generator of dicts, parsing json, too
     projects_and_contributors = ({"project_name": record['project_name'],
                                   "contributors": json.loads(record['contributors'])}
                                  for record in select_query_result)
 
     merge_query = """
                   WITH $json as j
-                  UNWIND j.contributors as contributors
-                  FOREACH (contributor in contributors |
-                  MERGE (c:Contributor {uuid: apoc.convert.toInteger(contributor.uuid)})
-                  ON CREATE SET c.name=contributor.name,
-                  c.github_id=contributor.github_id,
-                  c.login=contributor.login,
-                  c.host_type=contributor.host_type)
+                  MERGE (c:Contributor {uuid: apoc.convert.toInteger(j.uuid)})
+                  ON CREATE SET c.name=j.name,
+                  c.github_id=j.github_id,
+                  c.login=j.login,
+                  c.host_type=j.host_type
                   """
     match_merge_query = """
                         WITH $json as j
-                        UNWIND j.contributors as contributors
                         MATCH (p:Project {name: $name})
-                        FOREACH (contributor in contributors |
-                            MERGE (c:Contributor {uuid: apoc.convert.toInteger(contributor.uuid)})
-                            MERGE (c)-[:CONTRIBUTES_TO]->(p)
-                        )
+                        MATCH (c:Contributor {uuid: apoc.convert.toInteger(j.uuid)})
+                        MERGE (c)-[:CONTRIBUTES_TO]->(p)
                         """
     authentication = (args.neo4j_user,
                       os.environ.get('GRAPHDBPASS') or getpass('Graph DB Pass:'))
     neo4j_driver = get_neo4j_driver(args.neo4j_URI, authentication)
-    output = []
     with connect(args.DB) as conn:
         for p_c in projects_and_contributors:
-            project_name, contributors = (p_c['project_name'], p_c['contributors'])
+            # Unpack dict from gen for easy passing
+            p, contributors = (p_c['project_name'], p_c['contributors'])
             logger.info(f"Beginning MERGEing the {len(contributors)} "
-                        f"contributors to project '{project_name}'")
+                        f"contributors to project '{p}'")
             return_codes = []
-            for i, contributor in enumerate(contributors, 1):
+            # For each `contributors` list, iterate its dicts
+            for i, c in enumerate(contributors, 1):
                 logger.debug(f"MERGEing contributor {i} "
                              f"(out of {len(contributors)}) for project "
-                             f"{project_name} using query:"
-                             f"\n{merge_query}\n")
+                             f"{p} using query:\n{merge_query}\n")
                 return_code = \
                     merge_contributors(neo4j_driver, merge_query,
-                                       merge_query_params={"json": contributor},
+                                       merge_query_params={"json": c},
                                        match_query=match_merge_query,
-                                       match_query_params={"json": contributor,
-                                                           "name": project_name})
-                update_query = f"""UPDATE project_names SET
-                                merge_contributors_succeeded=?
-                                WHERE project_name='{project_name}'"""
+                                       match_query_params={"json": c, "name": p})
                 return_codes.append(return_code)
-            else:
-                logger.info(f"Finished MERGEing the {len(contributors)} "
-                            f"contributors to project '{project_name}'")
 
+            # Once all contributors have attempted to MERGE, try to update
+            # SQLite
+            logger.info(f"Finished MERGEing the {len(contributors)} "
+                        f"contributors to project '{p}'")
+            update_query = f"""UPDATE project_names SET
+                            merge_contributors_succeeded=?
+                            WHERE project_name='{p}'"""
+
+            # Have a 1-or-0 code for the whole project completing MERGE
+            # succesfully, because all the contributors are to be treated as a
+            # batch; unless the whole batch is merged correctly, it's a no-go
             if all(code==0 for code in return_codes):
                 out = execute_sqlite_query(conn, update_query, (1,))
             else:
@@ -84,10 +85,10 @@ def main():
 
             if out is None:
                 logger.warning("SQLite record corresponding to "
-                                f"'{project_name}' DID NOT UPDATE")
+                                f"'{p}' DID NOT UPDATE")
             else:
                 logger.info("SQLite record corresponding to "
-                            f"'{project_name}' updated successfully")
+                            f"'{p}' updated successfully")
 
 
 if __name__ == "__main__":
