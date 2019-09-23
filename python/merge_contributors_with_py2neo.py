eb020d7c95a2a5b63967521cfa0296f0cd03cfe5
ebb-earl-co <eberlcoecolin@gmail.com>
2019-09-23 16:32:17 -0700

    Add finishing 'for' structure

--- python/merge_contributors_with_py2neo.py
+++ python/merge_contributors_with_py2neo.py
@@ -3,8 +3,11 @@
 
 import os
 import sys
+from functools import reduce
+from traceback import print_tb
 
 from py2neo import Graph, Node
+from py2neo.database import GraphError
 
 GRAPHDBPASS = 'GRAPHDBPASS'
 
@@ -21,8 +24,29 @@ def get_graph_password(env_variable_name=GRAPHDBPASS):
 
 
 def execute_cypher_match_statement(g, statement, **kwargs):
+    """ Return the iterator of query results from `py2neo.Graph.run`
+    Args:
+        g (py2neo.Graph): the graph object representing DB to interact with
+        statement (str): the query to run on `g`
+        (kwargs): parameters that are inserted into `statement`
+    Returns:
+        (py2neo.database.Cursor): query results
+    """
     cursor = g.run(statement, **kwargs)
     return cursor
+
+
+def update_Node_and_return(node, **kwargs):
+    """ Call `node`.update, inserting {k: v for k, v in kwargs.items()}
+    into the `node`'s internal values() dict. Returns `node`, udpated.
+    Args:
+        (node): py2neo.Node to update
+        (kwargs): key, value pairs of properties to add to `node`
+    Returns:
+        (node): `node` passed in with its internal properties changed
+    """
+    node.update(**kwargs)
+    return node
 
 
 def return_Node_from_record(label, record):
@@ -42,8 +66,8 @@ def return_Node_from_record(label, record):
 def main():
     pw = get_graph_password()
     if pw is None:
-        # print(f"Environment variable {GRAPHDBPASS} not set. Cannot access graph DB",
-        #       file=sys.stderr)
+        print(f"Environment variable {GRAPHDBPASS} not set. Cannot access graph DB",
+              file=sys.stderr)
         sys.exit(1)
     else:
         g = Graph(password=pw)
@@ -53,19 +77,76 @@ def main():
         <-[:IS_WRITTEN_IN]-(p:Project)
         <-[:HOSTS]-(:Platform {name: 'Pypi'})
         return p"""
+
+    tx = g.begin(autocommit=False)
+    print("Querying Neo4j for nodes representing Python projects on Pypi\n",
+          file=sys.stderr)
     python_projects_on_pypi = \
         execute_cypher_match_statement(g, python_projects_on_pypi_query)
 
-    python_projects_on_pypi_dicts = \
-        (dict(record) for record in python_projects_on_pypi)
-
+    print("Converting py2neo Cursor into generator of dicts\n", file=sys.stderr)
     python_projects_on_pypi_nodes = \
-        (return_Node_from_record("Project", **d)
-         for d in python_projects_on_pypi_dicts)
+        map(lambda record: record.get('p'), python_projects_on_pypi)
 
+    print("Setting the 'merged_contributors' property to -1 on py2neo.Node "
+          "objects representing Python projects\n", file=sys.stderr)
     python_projects_on_pypi_nodes_property_set = \
-        map(lambda n: n.setattr('merged_contributors', -1),
+        map(lambda n: update_Node_and_return(n, merged_contributors=-1),
             python_projects_on_pypi_nodes)
+
+    output_messages = ("\tProject '%s' updated UNSUCCESSFULLY\n",
+                       "\tProject '%s' updated SUCCESSFULLY\n")
+
+    num_successful, num_attempted = 0, 0
+    print("Attempting to push updates to remote graph\n",
+            file=sys.stderr)
+    for node in python_projects_on_pypi_nodes_property_set:
+        successful = 0
+        try:
+            tx.merge(node)
+        except GraphError as ge:
+            successful = 0
+            print_tb(ge)
+            continue
+        except Exception as e:
+            successful = 0
+            print_tb(e)
+            continue
+        else:
+            successful = 1
+            tx.commit()
+        finally:
+            num_successful += successful
+            num_attempted += 1
+            print(output_messages[successful] % node['name'], file=sys.stderr)
+    else:
+        print(f"{round((num_successful / num_attempted) * 100, 2)}% "
+                "successfully pushed to remote graph")
+        del tx
+
+    # print("Combining the generator of py2neo.Node objects into a py2neo.Subgraph\n",
+    #       file=sys.stderr)
+    # python_projects_on_pypi_subgraph = \
+    #     reduce(lambda node1, node2: node1 | node2,  # union (set operation)
+    #            python_projects_on_pypi_nodes_property_set)
+    # with g.begin(autocommit=False) as tx:
+    #     print("Attempting to push these updates to remote graph\n",
+    #           file=sys.stderr)
+    #     try:
+    #         tx.push(next(python_projects_on_pypi_subgraph))  # is 1 Subgraph obj
+    #     except GraphError as ge:
+    #         tx.rollback()
+    #         print(f"Exception occurred:\n{ge}", file=sys.stderr)
+    #         sys.exit(1)
+    #     except Exception as e:
+    #         tx.rollback()
+    #         sys.exit(e)
+    #     else:
+    #         print("The 'merged_contributors' property of Nodes representing "
+    #               "Python projects on Pypi were updated successfully",
+    #               file=sys.stderr)
+    #         tx.commit()
+    # del g
 
 
 if __name__ == "__main__":
