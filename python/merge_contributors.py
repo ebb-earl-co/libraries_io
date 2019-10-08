4215bf98b162ece40e4346467099bc89ad99a1c8
ebb-earl-co <eberlcoecolin@gmail.com>
2019-10-08 12:31:11 -0600

    Change two files:
    merge_contributors.py: add typing, defaultdict for contributor counting, batching logic
    request_libraries_io_load_sqlite.py: use partial in executing sqlite queries

--- python/merge_contributors.py
+++ python/merge_contributors.py
@@ -5,12 +5,13 @@ import itertools as it
 import json
 import os
 import sys
-from typing import Iterator, List, Tuple
+from collections import defaultdict
+from typing import ByteString, Dict, Generator, Iterator, List, Text, Tuple
 
 from py2neo import Graph, Node, Relationship
 from py2neo.database import Cursor
 
-from utils.utils import connect, Row
+from utils.utils import chunk, connect, Row
 
 GRAPHDBPASS = 'GRAPHDBPASS'
 
@@ -39,20 +40,30 @@ def execute_cypher_match_statement(g: Graph, statement: str, **kwargs) -> Cursor
     return cursor
 
 
-def create_contributor_node(label: str = "Contributor", **kwargs) -> Node:
-
+def create_contributor_node(d: Dict, label: str = "Contributor") -> Node:
+    """ Using the k, v pairs in `d`, create a Node object with those properties.
+    Takes k, as-is except for 'uuid', which is cast to int.
+    Args:
+        d (dict): property k, v pairs
+        label (str): The py2neo.Node.__primarylabel__ to assign
+    Returns:
+        (Node): py2neo.Node instance of type `label`
+    """
+    uuid = int(d.get('uuid', -1))
     contributor = Node("Contributor",
-                        uuid=int(c['uuid']),
-                        name=c['name'],
-                        github_id=c['github_id'],
-                        login=c['login'],
-                        host_type=c['host_type'])
+                       uuid=uuid,
+                       name=d.get('name'),
+                       github_id=d.get('github_id'),
+                       login=d.get('login'),
+                       host_type=d.get('host_type'))
+    return contributor
+
 
 def main(argv=None):
     if argv is None:
         argv: list = sys.argv
 
-    DB, merged_contributors = argv[1:]
+    DB, merged_contributors, batch_size = argv[1:]
     g: Graph = Graph(password=get_graph_password())
     python_projects_on_pypi_query: str = \
         """MATCH (:Language {name: 'Python'})
@@ -65,12 +76,12 @@ def main(argv=None):
         and project_name=?"""
 
     # Phase 1: get all project_names from Neo4j the contributors of which have
-    # not tried to be merged yet; this involves getting the `contributors` field
+    # not tried to be merged yet; this means getting the `contributors` field
     # from SQLite
     print("Querying Neo4j for nodes representing Python projects on Pypi\n",
           file=sys.stderr)
     projects_cursor: Cursor = execute_cypher_match_statement(
-            g, python_projects_on_pypi_query % int(merged_contributors)
+        g, python_projects_on_pypi_query % int(merged_contributors)
     )
 
     print("Converting py2neo Cursor into iterable of dicts\n", file=sys.stderr)
@@ -79,9 +90,10 @@ def main(argv=None):
     with connect(DB) as conn:
         conn.row_factory = Row
         nodes_and_execute_args: Generator[Tuple[Node, Tuple[Text, Tuple[Text]]]] = \
-            ((node, (select_contributors_query, (node.get("name"),))) for node in projects_nodes)
+            ((node, (select_contributors_query, (node.get("name"),)))
+             for node in projects_nodes)
+
         nodes_and_contributors_cursors: Iterator[Tuple[Node, Cursor]] = \
-            # Cursor type is really Iterator[Row]
             map(lambda tup: (tup[0], conn.execute(*tup[1])), nodes_and_execute_args)
 
         nodes_and_contributors_generators: Iterator[Tuple[Node, Generator[ByteString]]] = \
@@ -92,7 +104,7 @@ def main(argv=None):
 
         nodes_and_contributors_iterables: Iterator[Tuple[Node, Iterator[Dict]]] = \
             map(lambda tup: (tup[0], it.chain.from_iterable(tup[1])),
-                nodes_and_non_empty_list_iterables)
+                nodes_and_contributors_dict_iterators)
 
         node_and_contributor_pairs: Iterator[Iterator[Tuple[Node, Dict]]] = \
             map(lambda tup: zip(it.repeat(tup[0]), tup[1]), nodes_and_contributors_iterables)
@@ -104,105 +116,35 @@ def main(argv=None):
             map(lambda tup: (tup[0], create_contributor_node(tup[1])),
                 all_nodes_and_contributors)
 
-        grouped_pnodes_cnodes: Iterator[Node, Iterator[Node]] = \
-            it.groupby(project_nodes__contributor_nodes, lambda tup: tup[0])
-
-        batches = partition(grouped_pnodes_cnodes, 1000)
+        batches = chunk(project_nodes__contributor_nodes, int(batch_size))
+        projects = defaultdict(int)
         for batch in batches:
             tx = g.begin(autocommit=False)
-            merge_contributor = partial(merge_contributor, tx)
-            for groupby in batch:
-                all_merged = []
-                for pnode, cnodes in groupby:
-                    print(f"MERGEing contributors to Neo4j for project {pnode['name']}",
-                          file=sys.stderr)
-                    for cnode in enumerate(cnodes, 1):
-                        tx.merge(cnode, "Contributor", "uuid")
-                        rel = Relationship(cnode, "CONTRIBUTES_TO", pnode)
-                        tx.merge(rel)
-                        all_merged.append(True if g.exists(rel) else False)
-                    else:
-                        mc = 1 if all(all_merged) else 0
-                        print(f"{'NOT ALL' if mc == 0 else 'ALL'} "
-                              f"contributors to {project['name']} MERGEd successfully",
-                              file=sys.stderr)
-                        project.update(merged_contributors=mc)
-                        g.push(project)
+            for pnode_cnode in batch:
+                pnode, cnode = pnode_cnode
+                projects[pnode] += 1
+                print(f"MERGEing contributors to Neo4j for project {pnode['name']}",
+                      file=sys.stderr)
+                print(f"\tMERGEing contributor {projects[pnode]}: {cnode.get('name')}")
+                tx.merge(cnode, "Contributor", "uuid")
+                print("\t\tMERGEing relationship to contributor "
+                      f"{projects[pnode]}: {cnode.get('name')}")
+                rel = Relationship(cnode, "CONTRIBUTES_TO", pnode)
+                tx.merge(rel)
             else:
                 tx.commit()
+
+        for project, count in projects.items():
+            tx = g.begin(autocommit=False)
+            print("Upating 'merged_contributors' property for Project node "
+                  f"{project} with value {count}")
+            project.update(merged_contributors=count)
+            tx.push(project)
+            tx.commit()
         else:
             del g
+        print("\nFinished.", file=sys.stderr)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        projects_contributors = []
-        for node in projects_nodes:
-            cur = conn.cursor()
-            name = node['name']
-            print(f"SELECTing contributors from SQLite for project {name}\n",
-                  file=sys.stderr)
-            cur.execute(select_contributors_query, (name,))
-            result = cur.fetchall()
-            projects_contributors.append(json.loads(result['contributors']))
-            cur.close()
-
-    # Phase 2: Create a (:Contributor) node for each dict in the list resulting
-    # from json.loads().
-    for i, cs in enumerate(projects_contributors):
-        project = projects_nodes[i]
-        print(f"MERGEing contributors to Neo4j for project {project['name']}",
-              file=sys.stderr)
-        all_merged = (True,) if len(cs) == 0 else []
-        # If there are no contributors for a project, the below `for` loop will
-        # be passed over, but the `else` clause will run; in that case, have
-        # all_merged be an iterable that is truthy for all() so as to demarcate
-        # correctly having merged all (zero) contributors
-        for j, c in enumerate(cs, 1):
-            contributor = Node("Contributor",
-                               uuid=int(c['uuid']),
-                               name=c['name'],
-                               github_id=c['github_id'],
-                               login=c['login'],
-                               host_type=c['host_type'])
-            print(f"\tAttempting to MERGE contributor {j}: {contributor}",
-                  file=sys.stderr)
-            g.merge(contributor, "Contributor", "uuid")
-        # Phase 3: Create a relationship between (:Project) and (:Contributor) for
-        # each contributor that is in the SQLite record according to `project_name`
-            cp = Relationship(contributor, "CONTRIBUTES_TO", project)
-            print(f"\tAttempting to CREATE relationship to contributor {j}: {cp}",
-                  file=sys.stderr)
-            g.create(cp)
-            all_merged.append(True if g.exists(cp) else False)
-        else:
-            mc = 1 if all(all_merged) else 0
-            print(f"{'NOT ALL' if mc == 0 else 'ALL'} "
-                  f"contributors to {project['name']} MERGEd successfully",
-                  file=sys.stderr)
-            project.update(merged_contributors=mc)
-            g.push(project)
-    else:
-        del g
-
-
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()
