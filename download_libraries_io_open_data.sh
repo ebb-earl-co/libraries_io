09c1afe8eee2d78a1b3c76da3417b4b2911db190
Colin Eberl Coe <ebb-earl-co@pm.me>
2021-09-15 14:26:45 -0500

    Update download script for newest version

--- download_libraries_io_open_data.sh
+++ download_libraries_io_open_data.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
-# As of 2018-12-22, the most recent version of the libraries.io data is 1.4.0.
+# As of 2020-01-12, the most recent version of the libraries.io data is 1.6.0.
 # The below variables have defaults set for this version, so that this script
 # can be run without arguments if convenient; i.e. `. download_libraries_io_open_data.sh`
-# N.b. calling without arguments will download the 12.2 GB file into the PWD.
+# N.b. calling without arguments will download the 24.9 GB file into the PWD and
+# subsequently check the MD5 checksum of the downloaded file.
 
-URL=${1:-https://zenodo.org/record/2536573/files/Libraries.io-open-data-1.4.0.tar.gz?download=1}
-FILENAME=${2:-Libraries.io-open-data-1.4.0.tar.gz}
-CHECKSUM=${3:-5bf302e6944cb1a8283a316f65b24093}
+URL=${1:-https://zenodo.org/record/2536573/files/libraries-1.6.0-2020-01-12.tar.gz?download=1}
+FILENAME=${2:-libraries-1.6.0-2020-01-12.tar.gz}
+CHECKSUM=${3:-4f2275284b86827751bb31ce74238b15}
 
 curl -SL -XGET "$URL" --output "${FILENAME}"
 ACTUAL=$(md5 -q "${FILENAME}")
@@ -16,7 +17,7 @@ if [ "${ACTUAL}" = "${CHECKSUM}" ]; then
     2<&1 echo "${ACTUAL}"
     res=0
 else
-    2<&1 echo "Checksum did not match. Deleting ${FILENAME}"
+    2<&1 echo "Checksum does not match: Deleting ${FILENAME}"
     rm -f "${FILENAME}"
     res=1
 fi
